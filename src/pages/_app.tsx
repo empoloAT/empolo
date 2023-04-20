fb5dd378b4888bbaafcff0050aa8ae54b9291b27
Grappler2010 <yakhyaev2008@mail.ru>
2023-04-20 09:07:36 +0300

    change basic styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,40 +1,16 @@
-import "@fontsource/golos-ui";
-import type { GlobalState } from "little-state-machine";
-import { createStore, StateMachineProvider } from "little-state-machine";
-import { SessionProvider } from "next-auth/react";
+import "@fontsource/golos-ui";;
 import type { AppProps } from "next/app";
 import { FunctionComponent } from "react";
-import { SWRConfig } from "swr";
 import "../styles/globals.css";
-
-function log(store: GlobalState) {
-  return store;
-}
-
-const middleWares = process.env.NODE_ENV === "development" ? [log] : [];
-
-createStore(
-  {
-    scratchCode: "",
-    referralCode: "",
-    userDetails: { email: "", name: "" },
-    loginStep: "",
-  },
-  { middleWares }
-);
 
 const App: FunctionComponent<AppProps> = ({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: { ...pageProps },
 }) => {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <SessionProvider session={session}>
-      <StateMachineProvider>
-        <SWRConfig>{getLayout(<Component {...pageProps} />)}</SWRConfig>
-      </StateMachineProvider>
-    </SessionProvider>
+    getLayout(<Component {...pageProps} />)
   );
 };
 
