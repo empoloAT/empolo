0b6d0b3b13cdc588188ff0f3fc0cf17ac3af50fe
Grappler2010 <yakhyaev2008@mail.ru>
2023-05-19 02:17:18 +0300

    add toast for contactUs and footer forms

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,5 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const wordpressEndpoint = process.env.WORDPRESS_GRAPHQL_API_URL;
-
 export const client = new ApolloClient({
   uri: "https://empolo.at/graphql",
   cache: new InMemoryCache(),
