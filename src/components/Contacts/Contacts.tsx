5d8b664b3937c54d9a76c49b5512e219ecd576cd
Grappler2010 <yakhyaev2008@mail.ru>
2023-05-19 12:50:58 +0300

    fix telephone tad

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -5,7 +5,7 @@ import styles from "./contacts.module.scss";
 export const Contacts = () => {
   return (
     <address className={styles.component}>
-      <a className={styles.subtitle} href="+436601578989">+43660 157 8989</a>
+      <a className={styles.subtitle} href="tel:+436601578989">+43660 157 8989</a>
       <P className={styles.subtitle}>Eragasse 10 I 6900 Bregenz I AUT</P>
       <a className={styles.subtitle} href="mailto:marko.kalabota@empolo.at">marko.kalabota@empolo.at</a>
     </address>
