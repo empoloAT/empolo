4cac4fa64c9a82e55bff4779b1c355bb6ea2334a
Grappler2010 <yakhyaev2008@mail.ru>
2023-05-23 23:13:36 +0300

    fix font-size and change WP domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['empolo.at']
+    domains: ['content.empolo.digital']
   }
 }
 
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://empolo.at/graphql",
+  uri: "https://content.empolo.digital/graphql",
   cache: new InMemoryCache(),
 });
