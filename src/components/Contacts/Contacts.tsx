1a0d0ef634af6a05bc84c95a9dba19efe67b4246
empoloAT <133926962+empoloAT@users.noreply.github.com>
2023-06-02 11:15:01 +0200

    Update Contacts.tsx

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -5,9 +5,9 @@ import styles from "./contacts.module.scss";
 export const Contacts = () => {
   return (
     <address className={styles.component}>
-      <a className={styles.subtitle} href="tel:+436601578989">+43660 157 8989</a>
+      <a className={styles.subtitle} href="tel:+435572428608">+435572 428608</a>
       <P className={styles.subtitle}>Eragasse 10 I 6900 Bregenz I AUT</P>
-      <a className={styles.subtitle} href="mailto:marko.kalabota@empolo.at">marko.kalabota@empolo.at</a>
+      <a className={styles.subtitle} href="mailto:office@empolo.at">office@empolo.at</a>
     </address>
   );
 };
