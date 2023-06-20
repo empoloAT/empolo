967c5ea09f7fc41e4d1828020eae728b2ad3d3e2
Grappler2010 <yakhyaev2008@mail.ru>
2023-06-20 21:09:56 +0300

    fix mediaUrls from wordpress

--- src/utils/media.ts
+++ src/utils/media.ts
@@ -3,16 +3,16 @@ import { GET_ALL_MEDIA } from "../data/media";
 
 export const GetAllMedia = () => {
   const { data } = useQuery(GET_ALL_MEDIA);
-  const videoUrl = data?.mediaItems.nodes[7].mediaItemUrl;
-  const firstImageUrl = data?.mediaItems.nodes[8].mediaItemUrl;
+  const videoUrl = data?.mediaItems.nodes[8].mediaItemUrl;
+  const firstImageUrl = data?.mediaItems.nodes[0].mediaItemUrl;
   const secondImageUrl = data?.mediaItems.nodes[9].mediaItemUrl;
-  const thirdImageUrl = data?.mediaItems.nodes[6].mediaItemUrl;
-  const fourthImageUrl = data?.mediaItems.nodes[5].mediaItemUrl;
-  const fifthImageUrl = data?.mediaItems.nodes[4].mediaItemUrl;
-  const sixthImageUrl = data?.mediaItems.nodes[3].mediaItemUrl;
-  const seventhImageUrl = data?.mediaItems.nodes[2].mediaItemUrl;
-  const eighthImageUrl = data?.mediaItems.nodes[1].mediaItemUrl;
-  const ninthImageUrl = data?.mediaItems.nodes[0].mediaItemUrl;
+  const thirdImageUrl = data?.mediaItems.nodes[7].mediaItemUrl;
+  const fourthImageUrl = data?.mediaItems.nodes[6].mediaItemUrl;
+  const fifthImageUrl = data?.mediaItems.nodes[5].mediaItemUrl;
+  const sixthImageUrl = data?.mediaItems.nodes[4].mediaItemUrl;
+  const seventhImageUrl = data?.mediaItems.nodes[3].mediaItemUrl;
+  const eighthImageUrl = data?.mediaItems.nodes[2].mediaItemUrl;
+  const ninthImageUrl = data?.mediaItems.nodes[1].mediaItemUrl;
   const mediaUrls = {
     videoUrl,
     firstImageUrl,
